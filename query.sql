7675756b9175f2ba87d0bae5980243d1b665d03c
Patrick Dawkins <pjcdawkins@users.noreply.github.com>
2026-07-07 14:41:18 +0100

    Scale scores 10x

--- query.sql
+++ query.sql
@@ -10,7 +10,7 @@ RETURNING *;
 
 -- name: GetTotalScore :one
 WITH game_scores AS (
-    SELECT game.id, (10 * (11 - COUNT(guess.id))) AS score
+    SELECT game.id, (100 * (11 - COUNT(guess.id))) AS score
     FROM game
     INNER JOIN guess victory ON game.id = victory.game_id AND game.answer = victory.guess
     INNER JOIN guess ON game.id = guess.game_id
